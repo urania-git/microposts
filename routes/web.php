73e1f73fc9a464ee1fedf89a655789a7b93a2c91
yuta azuma <a.y.com1225@gmail.com>
2021-05-06 12:51:18 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+Route::get('/', 'MicropostsController@index');
+
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
@@ -22,4 +24,6 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
+
